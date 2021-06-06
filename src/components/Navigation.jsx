90556102c61fe9507fb25dddcfc1129e32a15493
listingslab <listingslab@gmail.com>
2021-06-06 15:28:16 +1000

    develops structure file

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,28 +8,45 @@ import {
 
 const useStyles = makeStyles((theme) => ({
   navigation: {
+    display: 'block',
+  },
+  section:{
+    display: 'block',
+    border: '1px solid green',
   },
 }))
 
 export default function Navigation() {
  
+  let showJSON = false
+
   const classes = useStyles()
   const docsifySlice = useSelector(state => state.docsify)
   const {
     config,
   } = docsifySlice
   if ( !config ) return null
-  // let showJSON = true
-  // const {
-  //   title,
-    
-  // } = config
+  const {
+    sections,
+  } = config
+  if ( !sections ) return null
 
   return <div className={ clsx( classes.navigation ) } >
               <Grid container>
-                <Grid item xs={ 12 } >
-                  Navigation
-                </Grid>                
+                { sections.map( (item, i) => {
+                    const { 
+                      name, 
+                    } = item
+                    return <Grid item xs={4} key={`aksd_${i}`}>
+                              <div className={ clsx( classes.section )}>
+                              { name }
+                            </div></Grid>
+                })}
+
+                  { showJSON ? <pre>
+                    { JSON.stringify( sections, null, 2 ) }
+                  </pre> : null }
+
               </Grid>
           </div>
 }
