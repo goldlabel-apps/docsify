aad413a69aebca647ac55cb95672f7055a6fe9d8
listingslab <listingslab@gmail.com>
2021-06-10 15:28:54 +1000

    Updates ContentTree

--- src/components/ContentTree.jsx
+++ src/components/ContentTree.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import TreeView from '@material-ui/lab/TreeView';
-import TreeItem from '@material-ui/lab/TreeItem';
-import Typography from '@material-ui/core/Typography';
-import MailIcon from '@material-ui/icons/Mail';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Label from '@material-ui/icons/Label';
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import InfoIcon from '@material-ui/icons/Info';
-import ForumIcon from '@material-ui/icons/Forum';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
+import TreeView from '@material-ui/lab/TreeView'
+import TreeItem from '@material-ui/lab/TreeItem'
+import Typography from '@material-ui/core/Typography'
+import MailIcon from '@material-ui/icons/Mail'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Label from '@material-ui/icons/Label'
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import InfoIcon from '@material-ui/icons/Info'
+import ForumIcon from '@material-ui/icons/Forum'
+import LocalOfferIcon from '@material-ui/icons/LocalOffer'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
+import ArrowRightIcon from '@material-ui/icons/ArrowRight'
 
 const useTreeItemStyles = makeStyles((theme) => ({
   root: {
     color: theme.palette.text.secondary,
     '&:hover > $content': {
-      backgroundColor: theme.palette.action.hover,
+      backgroundColor: 'none',
     },
     '&:focus > $content, &$selected > $content': {
-      backgroundColor: `var(--tree-view-bg-color, ${theme.palette.grey[400]})`,
+      backgroundColor: 'none',
       color: 'var(--tree-view-color)',
     },
     '&:focus > $content $label, &:hover > $content $label, &$selected > $content $label': {
@@ -62,11 +62,12 @@ const useTreeItemStyles = makeStyles((theme) => ({
     fontWeight: 'inherit',
     flexGrow: 1,
   },
-}));
+}))
 
-function StyledTreeItem(props) {
-  const classes = useTreeItemStyles();
-  const { labelText, labelIcon: LabelIcon, labelInfo, color, bgColor, ...other } = props;
+function StyledTreeItem( props) {
+
+  const classes = useTreeItemStyles()
+  const { labelText, labelIcon: LabelIcon, labelInfo, color, bgColor, ...other } = props
 
   return (
     <TreeItem
@@ -95,7 +96,7 @@ function StyledTreeItem(props) {
       }}
       {...other}
     />
-  );
+  )
 }
 
 StyledTreeItem.propTypes = {
@@ -104,7 +105,7 @@ StyledTreeItem.propTypes = {
   labelIcon: PropTypes.elementType.isRequired,
   labelInfo: PropTypes.string,
   labelText: PropTypes.string.isRequired,
-};
+}
 
 const useStyles = makeStyles({
   root: {
@@ -112,22 +113,25 @@ const useStyles = makeStyles({
     flexGrow: 1,
     maxWidth: 400,
   },
-});
+})
 
-export default function GmailTreeView() {
-  const classes = useStyles();
+export default function ContentTree () {
+  const classes = useStyles()
 
   return (
     <TreeView
-      className={classes.root}
+      className={ classes.root }
       defaultExpanded={['3']}
       defaultCollapseIcon={<ArrowDropDownIcon />}
       defaultExpandIcon={<ArrowRightIcon />}
       defaultEndIcon={<div style={{ width: 24 }} />}
     >
+    
       <StyledTreeItem nodeId="1" labelText="All Mail" labelIcon={MailIcon} />
       <StyledTreeItem nodeId="2" labelText="Trash" labelIcon={DeleteIcon} />
       <StyledTreeItem nodeId="3" labelText="Categories" labelIcon={Label}>
+        
+
         <StyledTreeItem
           nodeId="5"
           labelText="Social"
@@ -163,5 +167,5 @@ export default function GmailTreeView() {
       </StyledTreeItem>
       <StyledTreeItem nodeId="4" labelText="History" labelIcon={Label} />
     </TreeView>
-  );
+  )
 }
